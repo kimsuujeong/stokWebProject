764cfcff02a15075534aa9012ebd5b9f15fc7a13
sujeong <thehapy0103@naver.com>
2024-05-19 22:31:28 +0900

    application.properties / ignore적용

--- src/main/stock-react/src/page/Write.jsx
+++ src/main/stock-react/src/page/Write.jsx
@@ -1,27 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Form from 'react-bootstrap/Form';
-import ReactQuill from "react-quill";
-
+import ReactQuill from "react-quill"
 
 function Write() {
 
-	useEffect(() => {
-		const imageBtn = document.querySelector(".ql-image");
+	// useEffect
 
-		if (imageBtn) {
-			imageBtn.addEventListener("click", handleImageClick);
-		}
+	// useEffect(() => {
+	// 	const imageBtn = document.querySelector(".ql-image");
 
-		return () => {
-			if (imageBtn) {
-				imageBtn.removeEventListener("click", handleImageClick);
-			}
-		};
+	// 	if (imageBtn) {
+	// 		imageBtn.addEventListener("click", handleImageClick);
+	// 	}
 
-	}, []);
+	// 	return () => {
+	// 		if (imageBtn) {
+	// 			imageBtn.removeEventListener("click", handleImageClick);
+	// 		}
+	// 	};
 
-	const handleImageClick = () => {
-	};
+	// }, []);
+
+	// const handleImageClick = () => {
+	// };
 
 
 	const modules = {
@@ -30,7 +31,7 @@ function Write() {
 				[{ header: [1, 2, 3, false] }],
 				[{ color: ["red", "pink", "blue", "yellow", "gray", "black"] }],
 				["bold", "underline"],
-				["image"],
+				// ["image"],
 			],
 		},
 	};
@@ -42,19 +43,21 @@ function Write() {
 				<h1>
 					글쓰기
 				</h1>
+
 				<Form.Select
 					style={{ width: "100%" }}>
 					<option>주식 코드</option>
 				</Form.Select>
 				<br></br>
+
 				<Form.Control
 					style={{ width: "100%" }}
 					type="text" placeholder="제목" />
 				<br></br>
+
 				<ReactQuill
 					style={{ margin: "5px", width: "auto", height: "500px" }}
 					modules={modules}
-					placeholder="사진 첨부파일을 올리면 챗GPT가 답변해줍니다"
 				/>
 			</Form>
 		</>
